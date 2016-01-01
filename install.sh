63c6e576c8dac6f296b442884573a1602dcf35c4
myamamoto88 <myamamoto@aiming-inc.com>
2016-01-01 16:13:29 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:myamamoto88/dotfiles.git
+
+DOT_FILES=(.vimrc .tmux.conf)
+
+for file in ${DOT_FILES[@]}
+do
+  ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+curl -fsSL https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | bash -x
