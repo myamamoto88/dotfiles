52527b476726a2da01cb0b5e50b8c8e422e0faef
myamamoto88 <myamamoto@aiming-inc.com>
2016-01-01 19:56:10 +0900

    スクリプトの実行する場所に関係なく、ホームディレクトリにインストールするようにした

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pushd $HOME
+
 git clone git@github.com:myamamoto88/dotfiles.git
 
 DOT_FILES=(.vimrc .tmux.conf)
@@ -8,3 +10,5 @@ for file in ${DOT_FILES[@]}
 do
   ln -fs $HOME/dotfiles/$file $HOME/$file
 done
+
+popd
